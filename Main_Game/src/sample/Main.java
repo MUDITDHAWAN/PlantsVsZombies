bfd42a1d0592579c4b8021f63bc3638a116f95d3
samikprakash <samikprakash@gmail.com>
2019-10-26 22:45:13 +0530

    first gui

---
+++ Main_Game/src/sample/Main.java
@@ -0,0 +1,41 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("new hello world");
+        Button button = new Button("click me");
+        button.setOnAction(new ClickEvent());
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(button);
+        Scene scene = new Scene(stackPane,1000,250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+class ClickEvent implements EventHandler<ActionEvent>{
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        System.out.println("it works!!!");
+    }
+}
