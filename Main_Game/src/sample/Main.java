df00fe140f62cb5e0f04e41a0d4598a28bf978d2
samikprakash <samikprakash@gmail.com>
2019-10-28 14:16:49 +0530

    try

--- Main_Game/src/sample/Main.java
+++ Main_Game/src/sample/Main.java
@@ -11,9 +11,12 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 import javax.swing.*;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
@@ -25,14 +28,20 @@ public class Main extends Application {
         primaryStage.setTitle("new hello world");
         String currentDirectory = System.getProperty("user.dir");
         System.out.println(currentDirectory);
-        String name = currentDirectory+ "\\pvz_images\\plant_gifs\\peashooter.gif";
-        Image image = new Image(new FileInputStream(name));
+        String peashooter = currentDirectory+ "\\pvz_images\\plant_gifs\\peashooter.gif";
+        Image image = new Image(new FileInputStream(peashooter));
         ImageView iv = new ImageView(image);
         iv.setX(350);
         iv.setY(0);
         iv.setFitHeight(50);
         iv.setFitWidth(50);
         iv.setPreserveRatio(true);
+
+        String main_video = currentDirectory + "\\pvz_images\\loading_video.mp4";
+        Media main_menu = new Media(currentDirectory + "\\pvz_images\\loading_video.mp4");
+        MediaPlayer play_video = new MediaPlayer(main_menu);
+        play_video.setAutoPlay(true);
+        
 
         Button button = new Button("click me now");
 
